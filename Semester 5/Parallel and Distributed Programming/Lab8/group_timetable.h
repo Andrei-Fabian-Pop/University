e86feb509c3d73e275f7d2522e5cea4a7325c2b3
Andrei-Fabian-Pop <andrei.fabian.pop@gmail.com>
2024-01-13 23:56:35 +0200

    PDP/Lab8: Added naive genetic timetable algorithm
    Signed-off-by: Andrei-Fabian-Pop <andrei.fabian.pop@gmail.com>

---
+++ Semester 5/Parallel and Distributed Programming/Lab8/group_timetable.h
@@ -0,0 +1,23 @@
+#ifndef LAB8_GROUP_TIMETABLE_H
+#define LAB8_GROUP_TIMETABLE_H
+
+#include <string>
+#include "configurations.h"
+
+class GroupTimetable {
+public:
+	explicit GroupTimetable(std::string name, GroupTimetable_t timetable);
+	explicit GroupTimetable(std::string name, std::vector<int> linerized_timetable);
+
+	[[maybe_unused]] GroupTimetable_t getTimeTable();
+	[[maybe_unused]] void setGroupTable(GroupTimetable_t groupTimetable);
+
+	void print_group_table();
+
+private:
+	std::string m_name;
+	GroupTimetable_t m_timeTable;
+};
+
+
+#endif //LAB8_GROUP_TIMETABLE_H
---
+++ Semester 5/Parallel and Distributed Programming/Lab8/main.cpp
@@ -0,0 +1,15 @@
+// 6. Generate a school timetable through genetic algorithms.
+#include "timetable_generator.h"
+
+int main() {
+	// TODO: mode all DS to the struct based one
+	for (int i = 0; i < 100; ++i) {
+		auto timetable_generator = TimetableGenerator();
+	}
+
+	auto timetable_generator = TimetableGenerator();
+	timetable_generator.printTimetables();
+
+	return 0;
+}
+
